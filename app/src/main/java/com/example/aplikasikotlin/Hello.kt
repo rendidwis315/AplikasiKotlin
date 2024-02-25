ec89f441eb32c02e187b97c4f1cf0d4456367315
rendidwis315 <151853254+rendidwis315@users.noreply.github.com>
2024-02-25 19:54:50 +0700

    Tugas Fungsi Dan BIodata

--- app/src/main/java/com/example/aplikasikotlin/Hello.kt
+++ app/src/main/java/com/example/aplikasikotlin/Hello.kt
@@ -2,17 +2,26 @@ package com.example.aplikasikotlin
 
 fun main(args: Array<String>){
     biodata("Rendi")
-    hobby("Boxing" , "Karena saya suka keributan")
+    hobby("GYM" , "karena saya ingin memiliki badan seperti Arnold Swainstrager")
 }
 
 fun biodata(Nama: String, Kelas: String ="2A"){
     val biodata ="""
+        -------------------------------
+        *        Biodata Saya         *
+        -------------------------------
         Nama: $Nama
         Kelas: $Kelas 
     """.trimIndent()
     print(biodata)
 }
 fun hobby(Nama: String, desc: String){
-    println("hobby: $Nama")
-    println(desc)
+    val hobby = """
+        -------------------------------
+        *        Hobby saya           *
+        -------------------------------
+    hobby = $Nama
+    desc = $desc
+    """
+    println(hobby)
 }
